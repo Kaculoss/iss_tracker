a16ff48ef99286ce2146df63a8fbbd3c1bea86be
Sani Alhassan <sani@kaculoss.tech>
2024-05-23 21:06:24 +0000

    update

--- src/@core/index.ts
+++ src/@core/index.ts
@@ -1,30 +1,37 @@
-import type { ThemeConfig, UserThemeConfig } from './types'
-import { RouteTransitions, Skins } from '@core/enums'
-import type { UserConfig as LayoutConfig } from '@layouts/types'
+import type { ThemeConfig, UserThemeConfig } from "./types";
+import { RouteTransitions, Skins } from "@core/enums";
+import type { UserConfig as LayoutConfig } from "@layouts/types";
 
 export const defineThemeConfig = (
-  userConfig: UserThemeConfig,
+  userConfig: UserThemeConfig
 ): { themeConfig: ThemeConfig; layoutConfig: LayoutConfig } => {
-  const localStorageTheme = localStorage.getItem(`${userConfig.app.title}-theme`)
-  const localStorageIsVerticalNavSemiDark = localStorage.getItem(`${userConfig.app.title}-isVerticalNavSemiDark`)
+  const localStorageTheme = localStorage.getItem(
+    `${userConfig.app.title}-theme`
+  );
+  const localStorageIsVerticalNavSemiDark = localStorage.getItem(
+    `${userConfig.app.title}-isVerticalNavSemiDark`
+  );
 
   const localStorageSkin = (() => {
-    const storageValue = localStorage.getItem(`${userConfig.app.title}-skin`)
+    const storageValue = localStorage.getItem(`${userConfig.app.title}-skin`);
 
-    return Object.values(Skins).find(v => v === storageValue)
-  })()
+    return Object.values(Skins).find((v) => v === storageValue);
+  })();
 
   const localStorageTransition = (() => {
-    const storageValue = localStorage.getItem(`${userConfig.app.title}-transition`)
+    const storageValue = localStorage.getItem(
+      `${userConfig.app.title}-transition`
+    );
 
-    return Object.values(RouteTransitions).find(v => v === storageValue)
-  })()
+    return Object.values(RouteTransitions).find((v) => v === storageValue);
+  })();
 
   return {
     themeConfig: {
       app: {
         title: userConfig.app.title,
         logo: userConfig.app.logo,
+        mark: userConfig.app.mark,
         contentWidth: ref(userConfig.app.contentWidth),
         contentLayoutNav: ref(userConfig.app.contentLayoutNav),
         overlayNavFromBreakpoint: userConfig.app.overlayNavFromBreakpoint,
@@ -32,7 +39,9 @@ export const defineThemeConfig = (
         theme: ref(localStorageTheme || userConfig.app.theme),
         isRtl: ref(userConfig.app.isRtl),
         skin: ref(localStorageSkin || userConfig.app.skin),
-        routeTransition: ref(localStorageTransition || userConfig.app.routeTransition),
+        routeTransition: ref(
+          localStorageTransition || userConfig.app.routeTransition
+        ),
         iconRenderer: userConfig.app.iconRenderer,
       },
       navbar: {
@@ -41,9 +50,15 @@ export const defineThemeConfig = (
       },
       footer: { type: ref(userConfig.footer.type) },
       verticalNav: {
-        isVerticalNavCollapsed: ref(userConfig.verticalNav.isVerticalNavCollapsed),
+        isVerticalNavCollapsed: ref(
+          userConfig.verticalNav.isVerticalNavCollapsed
+        ),
         defaultNavItemIconProps: userConfig.verticalNav.defaultNavItemIconProps,
-        isVerticalNavSemiDark: ref(localStorageIsVerticalNavSemiDark ? JSON.parse(localStorageIsVerticalNavSemiDark) : userConfig.verticalNav.isVerticalNavSemiDark),
+        isVerticalNavSemiDark: ref(
+          localStorageIsVerticalNavSemiDark
+            ? JSON.parse(localStorageIsVerticalNavSemiDark)
+            : userConfig.verticalNav.isVerticalNavSemiDark
+        ),
       },
       horizontalNav: {
         type: ref(userConfig.horizontalNav.type),
@@ -93,5 +108,5 @@ export const defineThemeConfig = (
         sectionTitlePlaceholder: userConfig.icons.sectionTitlePlaceholder,
       },
     },
-  }
-}
+  };
+};
